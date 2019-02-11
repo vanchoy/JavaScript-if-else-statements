8169c75605c573a1d6c782a52d84115065cd9d37
Ivan Stoyanov <super.ludiq@gmail.com>
2019-02-11 12:32:39 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+var name = 'Ivan';
+var age = 26;
+var isMarried = 'yes';
+
+if (isMarried === 'yes') {
+    //if(false)
+    console.log(name + ' is married!');
+}
+else {
+    console.log(name + ' will hopefully marry soon :)');
+}
+
+isMarried = true;
+
+if(isMarried){
+    console.log('YES');
+}
+else {
+    console.log('NO!');
+}
+
+if(isMarried) {
+    console.log('YES!');
+}
+
+
+if ( 23 == '23') {
+    console.log('Something to print...');
+}
